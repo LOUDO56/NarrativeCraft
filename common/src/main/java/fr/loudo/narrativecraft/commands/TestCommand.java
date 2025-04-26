51db9c8d408200a34b6771c5df34b263bca7f5da
LOUDO <lucaslelouer@gmail.com>
2025-04-26 19:26:58 +0200

    Added: Fade out sound test (works!)

--- common/src/main/java/fr/loudo/narrativecraft/commands/TestCommand.java
+++ common/src/main/java/fr/loudo/narrativecraft/commands/TestCommand.java
@@ -8,13 +8,20 @@ import fr.loudo.narrativecraft.mixin.fields.DisplayFields;
 import fr.loudo.narrativecraft.mixin.fields.ItemDisplayFields;
 import fr.loudo.narrativecraft.screens.CutsceneSettingsScreen;
 import fr.loudo.narrativecraft.screens.KeyframeOptionScreen;
+import fr.loudo.narrativecraft.utils.MathUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.*;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Display;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.joml.Quaternionf;
@@ -25,6 +32,9 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TestCommand {
+
+    static double t = 0;
+    static ScheduledExecutorService scheduler;
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("test")
@@ -39,7 +49,40 @@ public class TestCommand {
                 .then(Commands.literal("keyframeDisplay")
                         .executes(TestCommand::createKeyFrameItem)
                 )
+                .then(Commands.literal("sound")
+                        .then(Commands.literal("playFadeOut")
+                                .executes(TestCommand::playFadeOut)
+                        )
+                )
         );
+    }
+
+    private static int playFadeOut(CommandContext<CommandSourceStack> context) {
+
+        long startTime = System.currentTimeMillis();
+        long endTime = 20000L;
+        ResourceLocation resourceLocation = ResourceLocation.withDefaultNamespace("custom.distant_memory");
+        SoundEvent sound = SoundEvent.createVariableRangeEvent(resourceLocation);
+        context.getSource().sendSuccess(() -> Component.literal("Played sound, current volume to 0.5F!"), false);
+        SimpleSoundInstance simpleSoundInstance = new SimpleSoundInstance(sound.location(), SoundSource.MASTER, 1.0F, 1.0F, SoundInstance.createUnseededRandom(), true, 0, SoundInstance.Attenuation.NONE, (double)0.0F, (double)0.0F, (double)0.0F, true);
+        Minecraft.getInstance().getSoundManager().play(simpleSoundInstance);
+
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        Runnable task = () -> {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            t = Math.min((double) elapsedTime / endTime, 1.0);
+            float newVolume = (float) MathUtils.lerp(1, 0, t);
+            Minecraft.getInstance().getSoundManager().setVolume(simpleSoundInstance, newVolume);
+            context.getSource().sendSuccess(() -> Component.literal(String.format("New volume set to: %.2f", newVolume)), false);
+            if(t >= 1.0) {
+                scheduler.shutdown();
+                Minecraft.getInstance().getSoundManager().stop(simpleSoundInstance);
+            }
+        };
+        scheduler.scheduleAtFixedRate(task, 0, 50, TimeUnit.MILLISECONDS);
+
+        return Command.SINGLE_SUCCESS;
+
     }
 
     private static int openTestScreen(CommandContext<CommandSourceStack> context) {
